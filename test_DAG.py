05e729ee5022ae45e15028c874cbd2c39a9ecd27
kakuktamas <64551170+kakuktamas@users.noreply.github.com>
2021-06-23 11:10:12 +0200

    Update test_DAG.py

--- test_DAG.py
+++ test_DAG.py
@@ -13,26 +13,26 @@ from airflow.models import Variable
 # These args will get passed on to each operator
 # You can override them on a per-task basis during operator initialization
 import os
-import boto3
-from botocore.exceptions import ClientError
+#import boto3
+#from botocore.exceptions import ClientError
 
 
 
-def upload_to_s3(s3_endpoint,access_key,secretkey,source_folder,target_bucket, prefix=None):
-
-    # initialize connection
-    s3_client = boto3.client('s3', endpoint_url=s3_endpoint,aws_access_key_id=access_key , aws_secret_access_key=secretkey)
-
-    
-    for file in os.listdir(source_folder):
-        try:
-            filename_path = (os.path.join(source_folder,file))
-            print(filename_path)
-            response = s3_client.upload_file(filename_path, target_bucket,prefix+file)
-            return True
-        except ClientError as e:
-            print(e)
-            return False
+#def upload_to_s3(s3_endpoint,access_key,secretkey,source_folder,target_bucket, prefix=None):
+#
+#    # initialize connection
+#    s3_client = boto3.client('s3', endpoint_url=s3_endpoint,aws_access_key_id=access_key , aws_secret_access_key=secretkey)
+#
+#    
+#    for file in os.listdir(source_folder):
+#        try:
+#            filename_path = (os.path.join(source_folder,file))
+#            print(filename_path)
+#            response = s3_client.upload_file(filename_path, target_bucket,prefix+file)
+#            return True
+#        except ClientError as e:
+#            print(e)
+#            return False
 
 
 
@@ -42,7 +42,7 @@ default_args = {
     'email': ['ktomi88@gmail.com'],
     'email_on_failure':  False,
     'email_on_retry':  False,
-    'retries': 2,
+    'retries': 0,
     'retry_delay': timedelta(minutes=1),
 }
 with DAG(
@@ -54,12 +54,12 @@ with DAG(
     tags=['test'],
 ) as dag:
 
-    upload_files = PythonOperator( 
-    task_id='Pythpn_upload_files_local_to_s3',
-    python_callable= upload_to_s3,
-    provide_context=True,
-    op_kwargs={"s3_endpoint":"{{ var.value.s3_endpoint }}","access_key":"{{ var.value.s3_access_key }}","secretkey": "{{ var.value.s3_secret_key }}","source_folder": "{{ var.value.tmp_folder }}","target_bucket":"spark", "prefix":"test_data/"},
-    ) 
+   # upload_files = PythonOperator( 
+   # task_id='Pythpn_upload_files_local_to_s3',
+   # python_callable= upload_to_s3,
+   # provide_context=True,
+   # op_kwargs={"s3_endpoint":"{{ var.value.s3_endpoint }}","access_key":"{{ var.value.s3_access_key }}","secretkey": "{{ var.value.s3_secret_key }}","source_folder": "{{ var.value.tmp_folder }}","target_bucket":"spark", "prefix":"test_data/"},
+   # ) 
 
     spark_1 = SparkSubmitOperator( 
     task_id='Run_yellow_cab_job' ,
@@ -68,4 +68,4 @@ with DAG(
     packages="org.apache.hadoop:hadoop-aws:3.2.0",
     name='yellow_cab_test',)
  
-    spark_1 << upload_files
+   # spark_1 << upload_files
