212b5d9bccf3c542865679cf8cb7063e829becea
kakuktamas <64551170+kakuktamas@users.noreply.github.com>
2021-06-23 16:48:46 +0200

    Update test_spark.py

--- spark_codes/test_spark.py
+++ spark_codes/test_spark.py
@@ -10,7 +10,9 @@ spark_context = spark.sparkContext
 spark_context._jsc.hadoopConfiguration().set("fs.s3a.access.key", "B2JDY11NHXLI77PHSX4D") 
 spark_context._jsc.hadoopConfiguration().set("fs.s3a.secret.key", "XBbgD4eM8Su2B7AZVyTe4hKY2IR1Oz05QYYEvCaD") 
 spark_context._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-spark_context._jsc.hadoopConfiguration().set("spark.hadoop.fs.s3a.endpoint","http://s3-rook-ceph.apps.okdpres.alerant.org.uk")
+spark_context._jsc.hadoopConfiguration().set("fs.s3a.endpoint","http://s3-rook-ceph.apps.okdpres.alerant.org.uk")
+spark_context._jsc.hadoopConfiguration().set("fs.s3a.connection.ssl.enabled","false")
+spark_context._jsc.hadoopConfiguration().set("fs.s3a.path.style.access","true")
 
 
 ## Define Schema
